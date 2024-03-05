54f5513021e8816ccd5d5ce2c24930373059ba11
Vladimir <110543788+ShkerbsKababik@users.noreply.github.com>
2024-03-05 03:25:25 +0300

    1.0 release
    wrote a note on the work

--- TestovoeZadanie1/TestovoeZadanie1/Calculator.cs
+++ TestovoeZadanie1/TestovoeZadanie1/Calculator.cs
@@ -7,6 +7,8 @@ using TestovoeZadanie1.Figures;
 
 namespace TestovoeZadanie1
 {
+    // так как рантайм происходит у клиента и он выбирает фигуру для просчета то данный код происходит не в компайл-тайм
+    // надеюсь я понял вашу идею
     public static class Calculator
     {
         public static double GetSquare(IFigure figure)
